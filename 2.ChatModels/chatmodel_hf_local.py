463d4bab296f721c61948a48bb47a5ed740f705d
mohammad70623 <mohammad70623@gmail.com>
2026-02-01 23:28:30 +0600

    Implement local HuggingFace TinyLlama chat model

---
+++ 2.ChatModels/chatmodel_hf_local.py
@@ -0,0 +1,16 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline 
+
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs=dict(
+        temperature=0.01,
+        max_new_tokens=512
+    )
+
+)
+
+model = ChatHuggingFace(llm=llm)
+response=model.invoke("what is Daffodil International University?")
+print(response.content)
