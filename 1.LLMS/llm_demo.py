919c96255f6030c177c4882dff3dd8e93c133d39
mohammad70623 <mohammad70623@gmail.com>
2026-02-01 19:52:01 +0600

    Add LLM QA example using Groq

--- 1.LLMS/llm_demo.py
+++ 1.LLMS/llm_demo.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0)
-response = llm.invoke("what is the capital of bangladesh?")
+response = llm.invoke("what is daffodil international university?")
 print(response)
