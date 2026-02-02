7ba44bf45f6bdacd8801e3e6249eca086d9fb63b
mohammad70623 <mohammad70623@gmail.com>
2026-02-03 02:43:46 +0600

    Add PromptTemplate for structured research paper summary

--- Prompt/prompt_ui.py
+++ Prompt/prompt_ui.py
@@ -14,3 +14,20 @@ paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+#Template 
+template = PromptTemplate(
+    template="""
+    Please summarize the research paper titled "{paper_input}" with the following specifications: 
+    Explanation Style: {style_input}
+    Explanation Length: {length_input}
+    1.Mathematical Details:
+     - Include relevant equations and mathematical equations if present in the paper.
+     - Explain the mathematical concepts using simple , Intuitive code snippets where applicable.
+    2.Analogies:
+     - use relatable analogies to simplify complex idea.
+    If certain information is not available in the paper, response with: "Insufficient Information available" instead of guessing.
+    Ensure the summary is clear, accurate and aligned with the provided style and length.
+""", 
+    input_variables=["paper_input", "style_input", "length_input"]
+)
