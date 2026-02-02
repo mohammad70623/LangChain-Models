7cd620dfea876d0211f9a96e49cebed7bcf20518
mohammad70623 <mohammad70623@gmail.com>
2026-02-03 03:55:50 +0600

    Add chat history to support multi-turn conversation with ChatGroq

--- Prompt/chatbot.py
+++ Prompt/chatbot.py
@@ -4,10 +4,15 @@ load_dotenv()
 
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
 
+chat_history = []
 
 while True:
     user_input = input('You: ')
+    chat_history.append(user_input)
     if user_input == 'exit':
         break 
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
     print('AI', result.content)
+
+print("Chat ended.")
