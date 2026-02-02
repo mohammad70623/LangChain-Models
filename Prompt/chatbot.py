f7b2fe672933d2420d51dd37f3b33502e1c3a874
mohammad70623 <mohammad70623@gmail.com>
2026-02-03 03:41:59 +0600

    Add interactive while True loop for real-time AI chat

--- Prompt/chatbot.py
+++ Prompt/chatbot.py
@@ -4,3 +4,10 @@ load_dotenv()
 
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
 
+
+while True:
+    user_input = input('You: ')
+    if user_input == 'exit':
+        break 
+    result = model.invoke(user_input)
+    print('AI', result.content)
