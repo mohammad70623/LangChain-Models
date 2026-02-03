2b3789d52283d38c5cb6922c4a5def7e011db665
mohammad70623 <mohammad70623@gmail.com>
2026-02-04 01:50:14 +0600

    Create PromptTemplates and initialize StrOutputParser

--- Output_parsers/stroutput_parsers1.py
+++ Output_parsers/stroutput_parsers1.py
@@ -16,3 +16,20 @@ llm = HuggingFacePipeline.from_model_id(
 
 model = ChatHuggingFace(llm=llm)
 
+# 1st prompt -> Detailed Report
+template1 = PromptTemplate(
+    template="Write a detailed report on {topic}",
+    input_variables=["topic"]
+)
+
+# 2nd prompt -> Summary
+template2 = PromptTemplate(
+    template="Write a 5 line summary of the following text:\n{text}",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({"topic": "Daffodil International University"})
+print(result)
