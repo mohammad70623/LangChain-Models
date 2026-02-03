6034071a893bf1c3a0107e67558afbcb2388e77e
mohammad70623 <mohammad70623@gmail.com>
2026-02-03 10:08:32 +0600

    Prepare chat pipeline using LangChain message abstractions

---
+++ Prompt/message.py
@@ -0,0 +1,8 @@
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_groq import ChatGroq 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
+
