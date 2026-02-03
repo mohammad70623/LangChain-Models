2c4f831dde2203b5e7041d07eb552036d8e0beea
mohammad70623 <mohammad70623@gmail.com>
2026-02-03 15:36:05 +0600

    demonstrate advanced Pydantic fields and model serialization

--- Structured_output/pydantic_demo.py
+++ Structured_output/pydantic_demo.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 
 class Student(BaseModel):
-    name: str
-    
-new_student = {'name': 'Mohammad'}
-student = Student(**new_student)
-print(student)
 
+    name: str = 'Mohammad'
+    age: Optional[int] = None
+    email: EmailStr
+    cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
+
+
+new_student = {'age':'22', 'email':'mohammad70623@gmail.com'}
+
+student = Student(**new_student)
+
+student_dict = dict(student)
+
+print(student_dict['age'])
+
+student_json = student.model_dump_json()
