722ebf5ad2d27fc99e774b0fc137592cf6e04898
mohammad70623 <mohammad70623@gmail.com>
2026-02-04 12:34:17 +0600

    Setup HuggingFace LLM with ChatHuggingFace and import PydanticOutputParser

---
+++ Output_parsers/pydanticoutputparser.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+# Load model
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    device=-1,
+    pipeline_kwargs={
+        "temperature": 0.01,
+        "max_new_tokens": 512
+    }
+)
+
+model = ChatHuggingFace(llm=llm) 
