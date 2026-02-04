2a0ef5cde79e8a66d47b28610b6dee177a27d30b
mohammad70623 <mohammad70623@gmail.com>
2026-02-04 13:08:59 +0600

    Add PromptTemplate and chain with PydanticOutputParser

--- Output_parsers/pydanticoutputparser.py
+++ Output_parsers/pydanticoutputparser.py
@@ -3,22 +3,43 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 
-# Load model
+
 llm = HuggingFacePipeline.from_model_id(
     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     task="text-generation",
-    device=-1,
+    device=-1,  # CPU
     pipeline_kwargs={
         "temperature": 0.01,
         "max_new_tokens": 512
     }
 )
 
-model = ChatHuggingFace(llm=llm) 
+model = ChatHuggingFace(llm=llm)
 
 class Person(BaseModel):
-    name: str = Field(..., description="The name of the person")
-    age: int = Field(..., gt=18, description="The age of the person")
-    city: str = Field(..., description="The city where the person lives")
+    name: str = Field(description="The name of the person")
+    age: int = Field(gt=18, description="The age of the person")
+    city: str = Field(description="The city where the person lives")
 
 parser = PydanticOutputParser(pydantic_object=Person)
+
+
+template = PromptTemplate(
+    template="""
+Give the name, age, and city of a {place} from Asia
+Respond in strict JSON format with actual values:
+{{
+  "name": "...",
+  "age": ...,
+  "city": "..."
+}}
+""",
+    input_variables=["place"],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+
+chain = template | model | parser
+
+result = chain.invoke({'place': 'Bangladesh'})
+print(result.name, result.age, result.city)
