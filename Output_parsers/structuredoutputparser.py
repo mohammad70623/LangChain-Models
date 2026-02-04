3002af51028abc78ba2b4fc3be2e3dc8aef91c16
mohammad70623 <mohammad70623@gmail.com>
2026-02-04 11:55:35 +0600

    Initialize HuggingFace LLM with LangChain

---
+++ Output_parsers/structuredoutputparser.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, response_schema
+
+# Load model
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    device=-1,
+    pipeline_kwargs={
+        "temperature": 0.01,
+        "max_new_tokens": 512
+    }
+)
+
+model = ChatHuggingFace(llm=llm)
+
