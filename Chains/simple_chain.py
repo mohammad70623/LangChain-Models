4cecbfe0bee6a16c58b678cddb2ded50c54f96e8
mohammad70623 <mohammad70623@gmail.com>
2026-02-05 12:15:14 +0600

    add simple prompt-model-parser chain

--- Chains/simple_chain.py
+++ Chains/simple_chain.py
@@ -7,3 +7,14 @@ load_dotenv()
 
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
 
+prompt = PromptTemplate(
+    template = "Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({"topic": "Cricket"})
+print(result)
