ddc392c3c6c336b28b4f94f8da9566070ec45b5c
mohammad70623 <mohammad70623@gmail.com>
2026-02-05 12:41:31 +0600

    define prompts for notes, quiz, and merge workflow

--- Chains/parallel_chain.py
+++ Chains/parallel_chain.py
@@ -5,3 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
+
+prompt1  = PromptTemplate(
+    template = "Generate short and simple notes from the following text \n {text}",
+    input_variables=["text"]
+)
+prompt2 = PromptTemplate(
+    template = "Generate 5 short question answers from the following text:\n{text}",
+    input_variables=["text"]
+)
+
+prompt3 = PromptTemplate(
+    template="Merge provided notes and quize into a single document \n notes: {notes} \n quize: {quize}",
+    input_variables=["notes", "quize"]
+)
+
