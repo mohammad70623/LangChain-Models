5e5b26986253a114fbf85a6f9eed7ba4a8370848
mohammad70623 <mohammad70623@gmail.com>
2026-02-05 12:18:03 +0600

    configure Groq LLM and environment setup

---
+++ Chains/sequential_chain.py
@@ -0,0 +1,7 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
