6dc5e9f97e8f7df649503566adc3569483545ae0
mohammad70623 <mohammad70623@gmail.com>
2026-02-05 16:02:55 +0600

    Add Groq LLM sentiment classification with PydanticOutputParser

--- Chains/conditional_chain.py
+++ Chains/conditional_chain.py
@@ -3,16 +3,27 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel
 from dotenv import load_dotenv
-
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
 load_dotenv()
-model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
 
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
+parser = StrOutputParser()
+
+class Feedback(BaseModel):
+
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+ 
 prompt1 = PromptTemplate(
-    template= "Classify the sentimant of the following feedback text into positive and negetive \n {feedback}",
-    input_variables=["feedback"]
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
 )
 
-parser = StrOutputParser()
+classifier_chain = prompt1 | model | parser2
 
 classifier_chain = prompt1 | model | parser 
 print(classifier_chain.invoke({"feedback": "The product quality is excellent and delivery was prompt."}))
