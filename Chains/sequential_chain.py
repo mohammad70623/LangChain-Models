516d92f3afbafa07a6663b49c95e787f568112e0
mohammad70623 <mohammad70623@gmail.com>
2026-02-05 12:26:38 +0600

    add multi-step LangChain chain for report summarization

--- Chains/sequential_chain.py
+++ Chains/sequential_chain.py
@@ -5,3 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
+
+prompt1  = PromptTemplate(
+    template = "Generate a details report on {topic}",
+    input_variables=["topic"]
+)
+prompt2 = PromptTemplate(
+    template = "Generate 5 pointer summary from the following text:\n{text}",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+chain = prompt1 | model | parser | prompt2 | model | parser
+result = chain.invoke({"topic": "Unemployment in Bangladesh"})
+print(result)
