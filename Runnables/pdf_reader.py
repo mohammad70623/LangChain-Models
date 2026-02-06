dbafcc1e59ea29667f2cf983de7a01a6958ff8e2
mohammad70623 <mohammad70623@gmail.com>
2026-02-06 17:09:56 +0600

    load GROQ_API_KEY from .env for secure LLM access

---
+++ Runnables/pdf_reader.py
@@ -0,0 +1,12 @@
+# pdf_reader.py
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
