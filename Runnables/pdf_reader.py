fc3eed203cc6fd52ce8210e756161ffef3623154
mohammad70623 <mohammad70623@gmail.com>
2026-02-06 17:13:15 +0600

    load and read PDF using PyPDFLoader

--- Runnables/pdf_reader.py
+++ Runnables/pdf_reader.py
@@ -10,3 +10,7 @@ import os
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
+
+pdf_path = "/home/rafat/Mohammad/LangChain_Models/Runnables/MOHAMMAD_CV.pdf"
+loader = PyPDFLoader(pdf_path)
+documents = loader.load()
