35eef3b45e25257a4b1cc8458abc9b9fbf2be68d
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 22:02:05 +0600

    Add PyPDFLoader to load PDF documents

---
+++ Documents_loaders/pdf_loader.py
@@ -0,0 +1,10 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader('/home/Mohammad/LangChain_Models/Documents_loaders/dl-curriculum.pdf')
+
+docs = loader.load()
+
+print(len(docs))
+
+print(docs[0].page_content)
+print(docs[1].metadata)
