527db814c310a9ff406c2a68cded30ac107e1c27
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 19:03:40 +0600

    Implement parallel content generation chain

--- Runnables/runnable_parallel.py
+++ Runnables/runnable_parallel.py
@@ -2,8 +2,30 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableSequence
+from langchain_core.runnables import RunnableSequence, RunnableParallel
 
 load_dotenv() 
 
-model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024) 
+
+prompt1 = PromptTemplate(
+    template="Write a tweet about {topic}",
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template= "Write a LinkedIn post about {topic}",
+    input_variables=["topic"]
+)
+
+perser = StrOutputParser()
+
+
+parallel_chain = RunnableParallel(
+    {
+    "tweet": RunnableSequence(prompt1, model, perser),
+    "linkedin": RunnableSequence(prompt2, model, perser)
+    }
+)
+response = parallel_chain.invoke({"topic": "AI"})
+print(response)
