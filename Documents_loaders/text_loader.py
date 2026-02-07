632810c58478507dd8fb48dafce0f682393e5193
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 20:58:33 +0600

    Load text file and run ChatGroq chain for summarization

--- Documents_loaders/text_loader.py
+++ Documents_loaders/text_loader.py
@@ -16,3 +16,18 @@ prompt = PromptTemplate(
 
 parser = StrOutputParser()
 
+loader = TextLoader('cricket.txt', encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
