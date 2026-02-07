8e682c01bd01035ec5318522ba6186f3ff9076c8
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 18:43:03 +0600

    Add ChatGroq model setup with env loading

---
+++ Runnables/runnable_sequence.py
@@ -0,0 +1,10 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv() 
+
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
+
