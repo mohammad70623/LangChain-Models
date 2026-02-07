99a702931389d190e9fab2576e3b529561b6c753
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 19:15:12 +0600

    Add multi-step prompt templates example

--- Runnables/runnable_passthrough.py
+++ Runnables/runnable_passthrough.py
@@ -7,3 +7,16 @@ from langchain_core.runnables import RunnableSequence, RunnableParallel, Runnabl
 load_dotenv() 
 
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024) 
+
+prompt1 = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=["topic"]
+)
+
+perser = StrOutputParser() 
+
+prompt2 = PromptTemplate(
+    template= "Explain the following joke {text}",
+    input_variables=["text"]
+)
+
