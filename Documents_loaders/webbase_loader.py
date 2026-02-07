0ed4ff0345ec882f9ff6d977a796dd3c729a1f9a
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 22:44:30 +0600

    Set up ChatGroq model and environment for WebBaseLoader workflow

---
+++ Documents_loaders/webbase_loader.py
@@ -0,0 +1,10 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024) 
+
