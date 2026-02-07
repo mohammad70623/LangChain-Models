5b46c0deda2d02fa1086f0b3ab22e0eecd7f3c14
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 19:30:07 +0600

    Extend LangChain setup with RunnableLambda

---
+++ Runnables/runnable_lambda.py
@@ -0,0 +1,16 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
+
+load_dotenv() 
+
+model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024) 
+
+prompt = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=["topic"]
+)
+
+perser = StrOutputParser() 
