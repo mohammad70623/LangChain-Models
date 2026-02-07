719b66eb241e2eb6ac688e976223a02d75b1dcd5
mohammad70623 <mohammad70623@gmail.com>
2026-02-07 18:50:01 +0600

    Add RunnableSequence chain with prompt and output parser

--- Runnables/runnable_sequence.py
+++ Runnables/runnable_sequence.py
@@ -8,3 +8,12 @@ load_dotenv()
 
 model = ChatGroq(model="openai/gpt-oss-safeguard-20b", temperature=0, max_tokens=1024)
 
+prompt = PromptTemplate(
+    template= "Write a jokes about {topic}",
+    input_variables=["topic"],
+)
+perser = StrOutputParser()
+
+chain = RunnableSequence( prompt, model, perser)
+response = chain.invoke({"topic": "AI"})
+print(response)
